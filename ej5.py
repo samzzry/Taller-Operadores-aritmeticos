b6cfb333194e7984c861393f1bdaaf95b7accb7a
samzzry <samuelhumanez6@gmail.com>
2025-12-02 08:13:24 -0500

    ej5.py

--- ej5.py
+++ ej5.py
@@ -3,22 +3,16 @@
 def definir_radio():
     radio = float(input("Ingrese el radio del círculo: "))
     return radio
-
 def calcular_area(radio):
     area = 3.1416 * (radio ** 2)
     return area
-
 def imprimir_datos(radio):
     print("El radio es:", radio)
-
 def imprimir_resultado(area):
     print("El área del círculo es:", area)
-
-
 # Programa principal
 radio = definir_radio()
 area = calcular_area(radio)
-
 imprimir_datos(radio)
-
 imprimir_resultado(area)
+
