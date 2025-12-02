d0ca1bab1b83a469a392740adab85c02b6f2a2c3
samzzry <samuelhumanez6@gmail.com>
2025-12-02 07:45:44 -0500

    ej1.py

--- ej1.py
+++ ej1.py
@@ -3,27 +3,22 @@
 def definir_base():
     base = float(input("Ingrese la base del triángulo: "))
     return base
-
 def definir_altura():
     altura = float(input("Ingrese la altura del triángulo: "))
     return altura
-
 def calcular_area(base, altura):
     area = (base * altura) / 2
     return area
-
 def imprimir_datos(base, altura):
     print("La base es:", base)
     print("La altura es:", altura)
-
 def imprimir_resultado(area):
     print("El área del triángulo es:", area)
-
-
 # Programa principal
 base = definir_base()
 altura = definir_altura()
 area = calcular_area(base, altura)
 
 imprimir_datos(base, altura)
+
 imprimir_resultado(area)
