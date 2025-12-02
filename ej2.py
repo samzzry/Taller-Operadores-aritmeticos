2b0dddf0f5e906ecfee5e0034368ac317ee19200
samzzry <samuelhumanez6@gmail.com>
2025-12-02 08:25:06 -0500

    ej2.py

--- ej2.py
+++ ej2.py
@@ -1,16 +1,12 @@
-# Volumen de una esfera
-
+# volume de una esfera
 def definir_radio():
     radio = float(input("Ingrese el radio de la esfera: "))
     return radio
-
 def calcular_volumen(radio):
     volumen = (4/3) * 3.1416 * (radio ** 3)
     return volumen
-
 def imprimir_datos(radio):
     print("El radio es:", radio)
-
 def imprimir_resultado(volumen):
     print("El volumen de la esfera es:", volumen)
 
@@ -21,3 +17,4 @@ volumen = calcular_volumen(radio)
 
 imprimir_datos(radio)
 imprimir_resultado(volumen)
+
